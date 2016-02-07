75b2efc80de6b94caee57192f5f1f8e05016f659
David Asfaha <davidasfaha@gmail.com>
2016-02-07 18:12:22 +0000

    Started on ls

---
+++ day16/Ls.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+class Ls{
+
+    private String dir;
+
+    static public void main(String[] args ){
+        System.out.println("Hello!!!!");
+
+    }
+    
+    public void  Ls(String dir){
+        this.dir = dir;
+    }
+
+    public List<String> Run(){
+        List<String> retList = new ArrayList<String>();
+        return retList; 
+    }
+    
+}
---
+++ day16/LsTest.java
@@ -0,0 +1,24 @@
+import org.junit.*;
+import java.util.*;
+import static org.junit.Assert.*;
+
+public class LsTest {
+
+    List<String> files;
+
+    @BeforeClass 
+    public void initTest(){
+        Ls ls = new Ls();
+        files = ls.run();
+    }
+   
+    @Test
+    public void NumberOfFiles(){
+        assertEquals(files.length(), 2);
+    }
+
+    @Test
+    public void FilesName(){
+        assertTrue(files.contains("file1.test") && files.contains("file2.test"));
+    }
+}
