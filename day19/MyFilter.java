6759d8210d3b5243183d0f342fa1eb7c146bce71
David Asfaha <davidasfaha@gmail.com>
2016-05-06 11:31:21 +0100

    Before deleting class files

--- day19/MyFilter.java
+++ day19/MyFilter.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class MyFilter{
-    public static List<String> allMatches(List<String> toMatch, Predicate<String> pred){
-        List<String> returnList = new ArrayList();
-        for(String elem : toMatch){
+    public static <T> List<T> allMatches(List<T> toMatch, Predicate<T> pred){
+        List<T> returnList = new ArrayList();
+        for(T elem : toMatch){
             if(pred.test(elem))
                returnList.add(elem); 
         }    
